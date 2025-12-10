3b11eac234c420187fb9c00f1835924d5babf31e
ksphay <153393152+ksphay@users.noreply.github.com>
2025-12-10 10:37:58 +0800

    Add thread and output CLI options

--- include/RunAction.hh
+++ include/RunAction.hh
@@ -2,13 +2,14 @@
 #define RUN_ACTION_HH
 
 #include "G4UserRunAction.hh"
+#include "G4String.hh"
 
 class G4Run;
 
 class RunAction : public G4UserRunAction
 {
 public:
-    RunAction();
+    explicit RunAction(const G4String& outputFileName);
     ~RunAction() override;
 
     void BeginOfRunAction(const G4Run* run) override;
